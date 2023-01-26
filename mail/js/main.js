82d994857bc584930f7aed8ca2ab68af6ae039ad
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-01-26 17:24:07 +0100

    init project

---
+++ mail/js/main.js
@@ -0,0 +1,27 @@
+// Mail
+// Chiedi all'utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+//     stampa un messaggio appropriato sull'esito del controllo.
+
+
+//     Prima di partire a scrivere codice poniamoci qualche domanda
+// Che ci sia un array da qualche parte ?
+//     Se dobbiamo confrontare qualcosa che "cosa" ci serve ?
+//         Consigli del giorno:
+// scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+// si ma noi cosa vogliamo fare ?
+//     torniamo a scrivere in italiano
+// proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+
+
+// Creare un array con una lista di mail
+// Chiedi all'utente la sua email
+// controlla che sia nella lista di chi può accedere
+// stampa un messaggio appropriato sull'esito del controllo.
+
+
+const mails = [
+
+]
